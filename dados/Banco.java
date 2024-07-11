1736fc0e641620043d1cc198c4d719e675c4dc67
jairomachado07 <jr.hk22@gmail.com>
2024-07-11 11:21:46 -0300

    todas as alterações relaidas são para aprimoramento de versionamento em git

--- dados/Banco.java
+++ dados/Banco.java
@@ -3,7 +3,8 @@ package dados;
 public class Banco {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Venha você ser pelado também.");
+		System.out.println("Aqui você entra cliente de outros bancos e sai pelado.");
 
 	}
 
